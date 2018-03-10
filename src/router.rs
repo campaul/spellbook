aa0624adc07e9266c18625793a5724c1db112e13
Cameron Paul <cpaul37@gmail.com>
2018-03-09 19:34:12 -0600

    Add dummy router and do some reorganization

--- src/router.rs
+++ src/router.rs
@@ -2,64 +2,140 @@ use Context;
 use Handler;
 use Request;
 use Result;
-use Route;
-use Tween;
+use Response;
+use Middleware;
 
+use std;
+use std::collections::HashMap;
 use std::rc::Rc;
+use std::str::FromStr;
+
+pub struct Path {
+    params: HashMap<String, String>,
+}
+
+impl Path {
+    fn new(pattern: &String, path: &str) -> Option<Path> {
+        let match_segments: Vec<&str> = pattern.split("/").collect();
+        let path_segments: Vec<&str> = path.split("/").collect();
+
+        if match_segments.len() != path_segments.len() {
+            return None;
+        }
+
+        let mut params = HashMap::new();
+        let mut matches = true;
+        for i in 0..match_segments.len() {
+            let match_segment = match_segments[i];
+            let path_segment = path_segments[i];
+
+            if match_segment.starts_with(":") {
+                let mut match_string = String::from(match_segment);
+                match_string.remove(0);
+                let path_string = String::from(path_segment);
+                params.insert(match_string, path_string);
+                continue;
+            } else if match_segment == path_segment {
+                continue;
+            } else {
+                matches = false;
+            }
+        }
+
+        if matches {
+            return Some(Path {
+                params: params
+            });
+        } else {
+            return None
+        }
+    }
+
+    pub fn get<T: FromStr>(&self, key: &str) -> std::result::Result<T, &'static str> {
+        // TODO: Return a ValidationError instead of a str
+        match self.params.get(key) {
+            Some(s) => match s.parse() {
+                Ok(v) => Ok(v),
+                Err(_) => Err("value wrong type"),
+            },
+            None => Err("value does not exist"),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Route<S: Clone> {
+    pattern: String,
+    handler: Handler<S>,
+}
 
 #[derive(Clone)]
 pub struct Router<S: Clone> {
-    handlers: Vec<Handler<S>>,
-    tweens: Vec<Tween<S>>,
+    routes: Vec<Route<S>>,
+    middlewares: Vec<Middleware<S>>,
 }
 
 impl<S: Clone + 'static> Router<S> {
     pub fn new() -> Router<S> {
         Router {
-            handlers: vec![],
-            tweens: vec![],
+            routes: vec![],
+            middlewares: vec![],
         }
     }
 
-    pub fn proxy(handler: Handler<S>) -> Router<S> {
-        // TODO: this should proxy *all* verbs, not just get
-        Router::new().get("*", handler)
-    }
-
-    pub fn get(mut self, _path: &str, handler: Handler<S>) -> Router<S> {
-        self.handlers.push(handler);
+    pub fn get(mut self, pattern: &str, handler: Handler<S>) -> Router<S> {
+        self.routes.push(Route {
+            pattern: String::from(pattern),
+            handler: handler,
+        });
         self
     }
 
-    pub fn with(mut self, tween: Tween<S>) -> Router<S> {
-        self.tweens.insert(0, tween);
+    pub fn with(mut self, middleware: Middleware<S>) -> Router<S> {
+        self.middlewares.insert(0, middleware);
         self
     }
-}
 
-pub fn handle<S: Clone + 'static>(router: &Router<S>, state: S, req: Rc<Request>) -> Result {
-    // TODO: this is dummy code
-    let handler = router.handlers[0].clone();
-    let route = Rc::new(Route::new("/users/:user_id", req.uri()));
-    let context = Context {
-        state: state,
-        route: route,
-        req: req.clone(),
-    };
+    pub(crate) fn handle(&self, state: S, req: Rc<Request>) -> Result {
+        for route in self.routes.iter() {
+            if let Some(path) = Path::new(&route.pattern, req.path()) {
+                let context = Context {
+                    state: state,
+                    path: Rc::new(path),
+                    req: req.clone(),
+                };
+                let chain = build_chain(self.middlewares.clone(), Box::new(route.handler.clone()));
+                return chain(context);
+            }
+        }
 
-    let chain = build_chain(router.tweens.clone(), Box::new(handler));
-    chain(context)
+        let context = Context {
+            state: state,
+            path: Rc::new(Path {
+                params: HashMap::new(),
+            }),
+            req: req.clone(),
+        };
+
+        let chain = build_chain(vec![], Box::new(not_found));
+        chain(context)
+    }
 }
 
 fn build_chain<S: Clone + 'static>(
-    mut tweens: Vec<Tween<S>>,
+    mut middlewares: Vec<Middleware<S>>,
     next: Box<Fn(Context<S>) -> Result>,
 ) -> Box<Fn(Context<S>) -> Result> {
-    if tweens.len() == 0 {
+    if middlewares.len() == 0 {
         return next;
     }
 
-    let tween = tweens.pop().unwrap();
-    let chain = build_chain(tweens.clone(), next);
-    return Box::new(move |ctx: Context<S>| tween(ctx, &*chain));
+    let middleware = middlewares.pop().unwrap();
+    let chain = build_chain(middlewares.clone(), next);
+    return Box::new(move |ctx: Context<S>| middleware(ctx, &*chain));
+}
+
+fn not_found<S: Clone>(_: Context<S>) -> Result {
+    // TODO: set status code
+    Ok(Response::new().with_body("404 not found"))
 }
