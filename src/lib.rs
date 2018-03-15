15f5a2e0467e61b30be0a3258a624edd2860a231
Ben <ben.bariteau@gmail.com>
2018-03-15 12:47:48 -0700

    default query params to empty string

--- src/lib.rs
+++ src/lib.rs
@@ -216,10 +216,9 @@ impl<S: Clone> Context<S> {
     }
 
     /// Parses query params to a value
-    pub fn query_params<P>(&self) -> StdResult<P, Box<Error>>
+    pub fn query_params<P>(&self) -> StdResult<P, serde_urlencoded::de::Error>
         where for<'a> P: serde::Deserialize<'a> {
-        //TODO figure out how to use non-Box<Error> error here
-        let query_params_string = self.req.query().ok_or("no query params")?;
+        let query_params_string = self.req.query().unwrap_or("");
         let query_params: P = serde_urlencoded::from_str(query_params_string)?;
         Ok(query_params)
     }
